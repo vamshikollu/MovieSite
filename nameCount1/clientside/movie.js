6fe7c894f2976e070eb562409c2bbbb3995eb046
vamshikollu <vxk92780@ucmo.edu>
2017-06-01 11:53:20 -0700

    added new server as main /localhost.3000

--- nameCount1/clientside/movie.js
+++ nameCount1/clientside/movie.js
@@ -7,27 +7,87 @@ function tplawesome(e, t) {
 	}
 	return res
 }
+var a;
+var videoduration;
+//jquery onload will run this function
 
 $(function() {
 	
+	var propagateCards=function(resdata){
+		 console.log("called /reqmovlis.html from client sucess with res");
+		 console.log("response data is "+ resdata[1].mov_name);
+	
+		var a=resdata.length;
+		 for(var i=0;i<resdata.length;i++)
+			 {
+			 console.log(resdata[i]);
+			 var $elem = $("#results");
+			    $elem.append('<div class="card" style="width:20rem;"> <img class="card-img-top" src="'+resdata[i].mov_posterlink+'" alt="Card image cap">  <div class="card-block">    <div class="mov">'+resdata[i].mov_name+'</div>    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>    <a href="#" class="btn btn-primary">Go somewhere</a>  </div></div>');
+			 }
+	}
+
+	//step1: request the server for movie details
+	 $.ajax({
+		 type: 'GET',
+		 url:'/reqmovlis.html',
+		 //data: JSON.stringify(data),
+		 success: function(resdata){
+			 propagateCards(resdata);
+		
+				//step2: create and paste cards on body of main page
+			 // for each JSON object 
+			 
+
+		 },
+		 error:function(){
+			 alert("error sendind data to /reqmovlis.html");
+		 }
+	 })
+	  
+	 
+	$("#submitmovies").on("click", function(e) {
+		a = "telugu movies";
+		videoduration = "long";
+		request(e, a);
+
+		console.log(a);
+		console.log(e);
+		console.log(sort);
+	});
+
 	$("#submitttrailer").on("click", function(e) {
-		var a = "latest telugu movie trailers";
-		request(e,a);
+		a = "telugu trailers";
+		videoduration = "short";
+		request(e, a);
 	});
-	
-	$("#submithtrailer").on("click", function(e) {
-		var a = "latest hindi movie trailers";
-		request(e,a);
+
+	$("#submitsongs").on("click", function(e) {
+		a = "telugu songs";
+		videoduration = "any";
+		request(e, a);
 	});
-	
+
+	$("#submitcomedy").on("click", function(e) {
+		a = "telugu comedy short film";
+		videoduration = "medium";
+		sort = "relevance";
+		request(e, a);
+	});
 	$("form").on("submit", function(e) {
-		var a = $("#search").val();
-		request(e,a);
+
+		a = $("#search").val();
+
+		request(e, a);
 
 	});
-	
+
 	// $(window).on("resize", resetVideoHeight);
 });
+//console.log(a);
+
+//console.log(sort);
+
+
 
 function resetVideoHeight() {
 	$(".video").css("height", $("#results").width() * 3 / 4);
@@ -40,25 +100,34 @@ function init() {
 	});
 }
 
-function request(x,p) {
+// var sort=document.getElementById("filter").value;
+var sort = "ViewCount";
+
+function request(x, p) {
 	x.preventDefault();
 	// prepare the request
 	var request = gapi.client.youtube.search.list({
 		part : "snippet",
 		type : "video",
 		q : encodeURIComponent(p).replace(/%20/g, "+"),
-		maxResults : 5,
-		order : "viewCount",
-		publishedAfter : "2015-01-01T00:00:00Z"
+		maxResults : 10,
+		order : sort,
+		publishedAfter : "2016-01-01T00:00:00Z",
+		videoDuration : videoduration,
+		location : "17.3850,78.4867",
+		locationRadius : "999km",
+
 	});
 	// execute the request
+
 	request.execute(function(response) {
 		var results = response.result;
 		$("#results").html("");
 		$.each(results.items, function(index, item) {
 			$.get("item.html", function(data) {
 				$("#results").append(tplawesome(data, [ {
-					"videoid" : item.id.videoId
+					"videoid" : item.id.videoId,
+					"videotitle" : item.snippet.title
 				} ]));
 			});
 		});
--- nameCount1/serverrunner.js
+++ nameCount1/serverrunner.js
@@ -1,111 +1,188 @@
 var http = require('http');
-var fs=require("fs");
-var util=require("util");
-//var express=require("express");
-var mongodb=require("mongodb").MongoClient,format=require('util').format;
+var fs = require("fs");
+var util = require("util");
+var express = require("express");
+var app = express();
+var mongo = require("mongodb").MongoClient, format = require('util').format;
+var path = require('path');
 
+// creating server
 http.createServer(onRequest).listen(5245);
 console.log('Server running at http://localhost:5245/');
 
+// create express server
+app.listen(3000);
+console.log("express server started at http://localhost:3000/");
 
+// creating mongodb connection
+var dbres;
+mongo.connect('mongodb://127.0.0.1:27017/moviesDB', function(err, db) {
+	if (err) {
+		throw err;
+		console.log("failed to connect to moviesDB");
+	} else {
+
+		db.collection('list').find({}).toArray(function(err, list) {
+			if (err) {
+				throw err;
+			} else if (list.length) {
+				// print results
+				dbres=JSON.stringify(list);
+				console.log(dbres);
+			} else {
+				console.log("list not exist")
+			}
+			
+			db.close();
+			
+		});
+	}
+	
+});
+//console.log("the db value is :",dbres);
+
+// node routing
 function onRequest(req, res) {
-	var url=req.url;
-    //res.writeHead(200, {'Content-Type': 'text/plain'});
-   // res.writeHead('Hello vamshi from server\n');
-   
-    switch(url){
-    case '/':
-    	//console.log("A user made a request to:"+req.url);
-    	getStaticFileContent(res,"clientside/main.html",'text/html');
-    	//console.log("sucess home");
-    //	getStaticFileContent(res,"clientside/scripts.js",'html/javascript');
-       	break;
-       	
-    case '/scripts.js':
-    	//console.log("A user made a request to:"+req.url);
-      	getStaticFileContent(res,"clientside/scripts.js",'text/javascript');
-      	//console.log("sucess scripts");
-      	break;
-   // 	res.writeHead(404,{'content-type':'text/javascript'});
-   // 	res.end();
-      	
-    case '/movie.js':
-    	//console.log("A user made a request to:"+req.url);
-      	getStaticFileContent(res,"clientside/movie.js",'text/javascript');
-      	//console.log("sucess scripts");
-      	break;
-    case '/item.html':
-    	//console.log("A user made a request to:"+req.url);
-      	getStaticFileContent(res,"clientside/item.html",'text/html');
-      	//console.log("sucess scripts");
-      	break;
-    case '/info':
-    	console.log("info is called sucessfully"+req);
-    
-    	console.log('Request received: ');
-            var consData={'uday':0,'vamshi':1
-    	   	}
-      //     res.end(JSON.stringify(data));
-        req.on('data', function (request) {
-        	requestObject=JSON.parse(request);
-            console.log('Request DATA!::'+requestObject);
-         //   console.log("the data we resived is:"+JSON.stringify(data));
-            var t=consData[requestObject.name];
-           	console.log("name is"+requestObject.name);
-            
-            if(t==undefined)
-            	consData[requestObject.name]=1;
-            else
-            	consData[requestObject.name]=t+1;
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(consData));
-            
-        });
-        
-    	console.log("info ending");
-    //	res.send("hai you have reached server function");
-    	break;
-    	
-    case '/movie':
-    	getStaticFileContent(res,"clientside/movie.html",'text/html');
-    	break;
-    default:
-    	//console.log("default starting");
-        //console.log("A user made a request to:"+req.url);
-    	res.writeHead(404,{'content-type':'text/plain'});
-    	res.end("404-page not found");
-    	//console.log("default ending");
-    }    	
+	var url = req.url;
+	// res.writeHead(200, {'Content-Type': 'text/plain'});
+	// res.writeHead('Hello vamshi from server\n');
+	switch (url) {
+	case '/':
+		// console.log("A user made a request to:"+req.url);
+		getStaticFileContent(res, "clientside/main.html", 'text/html');
+		// console.log("sucess home");
+		// getStaticFileContent(res,"clientside/scripts.js",'html/javascript');
+		break;
+
+	case '/scripts.js':
+		// console.log("A user made a request to:"+req.url);
+		getStaticFileContent(res, "clientside/scripts.js", 'text/javascript');
+		// console.log("sucess scripts");
+		break;
+	// res.writeHead(404,{'content-type':'text/javascript'});
+	// res.end();
+
+	case '/movie.js':
+		// console.log("A user made a request to:"+req.url);
+		getStaticFileContent(res, "clientside/movie.js", 'text/javascript');
+		// console.log("sucess scripts");
+		break;
+	case '/item.html':
+		// console.log("A user made a request to:"+req.url);
+		getStaticFileContent(res, "clientside/item.html", 'text/html');
+		// console.log("sucess scripts");
+		break;
+	case '/info':
+		console.log("info is called sucessfully" + req);
+
+		console.log('Request received: ');
+		var consData = {
+			'uday' : 0,
+			'vamshi' : 1
+		}
+		// res.end(JSON.stringify(data));
+		req.on('data', function(request) {
+			requestObject = JSON.parse(request);
+			console.log('Request DATA!::' + requestObject);
+			// console.log("the data we resived is:"+JSON.stringify(data));
+			var t = consData[requestObject.name];
+			console.log("name is" + requestObject.name);
+
+			if (t == undefined)
+				consData[requestObject.name] = 1;
+			else
+				consData[requestObject.name] = t + 1;
+			res.writeHead(200, {
+				'Content-Type' : 'application/json'
+			});
+			res.end(JSON.stringify(consData));
+
+		});
+
+		console.log("info ending");
+		// res.send("hai you have reached server function");
+		break;
+
+	case '/movie':
+		getStaticFileContent(res, "clientside/movie.html", 'text/html');
+		break;
+	case '/images/logo.png':
+		getStaticFileContent(res, "images/logo.png", 'image/png');
+		break;
+	default:
+		// console.log("default starting");
+		// console.log("A user made a request to:"+req.url);
+		res.writeHead(404, {
+			'content-type' : 'text/plain'
+		});
+		res.end("404-page not found");
+		// console.log("default ending");
+	}
 }
 
+// express routing
 
 
-function getStaticFileContent(res,fp,contentType){
-	fs.readFile(fp,function(error,data){
-		if(error){
-			//console.log("fp error starting");
-			res.writeHead(500,{'Content-Type':'text/plain'});
-			res.end("500-Internal server error.");
-			//console.log("fp error ending");
-		}
-		if(data){
-			//console.log("fp sucess starting");
-			res.writeHead(200,{'Content-Type':'*/*'});
-			res.end(data);
-			//console.log("fp sucess ending");
-			//console.log("data is:"+data);
-		}
+app.get('/', function(req, res, next) {
+	console.log("express home calles");
 	
+	// res.send("hai from express node");
+	res.sendFile("movie.html", {
+		root : path.join(__dirname, './clientside')
+	});
+});
+app.get('/logo.png', function(req, res, next) {
+	res.sendFile("logo.png", {
+		root : path.join(__dirname, './images')
+	});
+});
+app.get('/movie.js', function(req, res, next) {
+	// res.send("hai from express node");
+	res.sendFile("movie.js", {
+		root : path.join(__dirname, './clientside')
+	});
+});
+app.get('/item.html', function(req, res, next) {
+	// res.send("hai from express node");
+	res.sendFile("item.html", {
+		root : path.join(__dirname, './clientside')
+	});
+});
+app.get('/reqmovlis.html',function(req,res,next){
+	console.log("/reqmovlis is called sucess fully");
+	res.writeHead(200,{'Content-type':'application/json'});
+	res.end(dbres);
+	
+})
+app.get('*', function(req, res) {
+	res.send("page not found", 404);
+});
+
+function getStaticFileContent(res, fp, contentType) {
+	fs.readFile(fp, function(error, data) {
+		if (error) {
+			// console.log("fp error starting");
+			res.writeHead(500, {
+				'Content-Type' : 'text/plain'
+			});
+			res.end("500-Internal server error.");
+			// console.log("fp error ending");
+		}
+		if (data) {
+			// console.log("fp sucess starting");
+			res.writeHead(200, {
+				'Content-Type' : '*/*'
+			});
+			res.end(data);
+			// console.log("fp sucess ending");
+			// console.log("data is:"+data);
+		}
+
 	});
 }
 
-//function infoProcessor(req,res){
-//	    res.send("hai you have reached server function");
-//		console.log("info is called sucessfully from function");
-//	}
+// function infoProcessor(req,res){
+// res.send("hai you have reached server function");
+// console.log("info is called sucessfully from function");
+// }
 
-
-
-
-
-	
